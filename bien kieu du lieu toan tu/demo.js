e77b001611b8a9a067e5696f7131666eb39043da
gunner4594 <duy.bk109@gmail.com>
2022-10-07 22:18:52 +0700

    Bien kieu toan tu du lieu

---
+++ bien kieu du lieu toan tu/demo.js
@@ -0,0 +1,34 @@
+
+
+// inputscorephysic=prompt("Enter the score physic");
+// inputscorechemistry=prompt("Enter the score chemistry");
+// inputscorebiological=prompt("Enter the score biological");
+//
+// let scorephysic = parseInt(inputscorephysic);
+// let scorechemistry = parseInt(inputscorechemistry);
+// let scorebiological = parseInt(inputscorebiological);
+// let totalScore = scorephysic + scorechemistry + scorebiological; //`${scorephysic}${scorechemistry}${scorebiological}`;
+// let averageScore = totalScore/3;
+//
+// document.writeln("The total score is:"+totalScore);
+// document.write("<br>");
+// document.writeln("The average score is:"+averageScore);
+
+// inputCelsius=prompt("Enter Celsius value");
+//
+// let Celsius= parseInt(inputCelsius);
+// let Fahrenheit = 9/5*Celsius + 32;
+//
+// document.write("The Fahrenheit value is:"+ Fahrenheit);
+
+// inputRadius=prompt("Enter Radius");
+// let Radius= parseInt(inputRadius);
+// let Area= Math.PI* Radius**2;
+//
+// document.write("The area value is:"+Area)
+
+// inputRadius=prompt("Enter Radius");
+// let Radius=parseInt(inputRadius);
+// let Perimeter= 2*Radius* Math.PI;
+//
+// document.write("The perimeter value is:"+Perimeter);
