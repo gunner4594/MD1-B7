f151be23e6f4fc939a79d47130e19946081846cb
gunner4594 <duy.bk109@gmail.com>
2022-10-11 15:57:22 +0700

    bo sung bai tap may tinh nang cao

--- Ung dung may tinh/main.js
+++ Ung dung may tinh/main.js
@@ -1,28 +1,46 @@
-// let number1 = document.getElementById('number1').value;
-// let number2 = document.getElementById('number2').value;
 
+// function add () {
+//     let number1 = +document.getElementById('number1').value;
+//     let number2 = +document.getElementById('number2').value;
+//     let cong = number1 + number2;
+//     document.getElementById('result').innerHTML = 'Result: ' + cong;
+// }
+// function subtraction () {
+//     let number1 = +document.getElementById('number1').value;
+//     let number2 = +document.getElementById('number2').value;
+//     let tru = number1 - number2;
+//     document.getElementById('result').innerHTML = 'Result: ' + tru;
+// }
+// function multiplication () {
+//     let number1 = +document.getElementById('number1').value;
+//     let number2 = +document.getElementById('number2').value;
+//     let nhan = number1*number2;
+//     document.getElementById('result').innerHTML = 'Result: ' + nhan;
+// }
+// function division () {
+//     let number1 = +document.getElementById('number1').value;
+//     let number2 = +document.getElementById('number2').value;
+//     let chia = number1/number2;
+//     document.getElementById('result').innerHTML = 'Result: ' + chia;
+// }
+//
 
-function add () {
-    let number1 = +document.getElementById('number1').value;
-    let number2 = +document.getElementById('number2').value;
-    let cong = number1 + number2;
-    document.getElementById('result').innerHTML = 'Result: ' + cong;
+let result = ''
+
+function xuLyDulieu(input) {
+    let a = document.getElementById(input).value;
+    result += a;
+    document.getElementById('result').innerHTML = result;
 }
-function subtraction () {
-    let number1 = +document.getElementById('number1').value;
-    let number2 = +document.getElementById('number2').value;
-    let tru = number1 - number2;
-    document.getElementById('result').innerHTML = 'Result: ' + tru;
+
+function xuLyketQua () {
+    let ketQua = eval(result);
+    document.getElementById('result').innerHTML = ketQua;
+    result = ketQua;
 }
-function multiplication () {
-    let number1 = +document.getElementById('number1').value;
-    let number2 = +document.getElementById('number2').value;
-    let nhan = number1*number2;
-    document.getElementById('result').innerHTML = 'Result: ' + nhan;
-}
-function division () {
-    let number1 = +document.getElementById('number1').value;
-    let number2 = +document.getElementById('number2').value;
-    let chia = number1/number2;
-    document.getElementById('result').innerHTML = 'Result: ' + chia;
+
+function xoa () {
+    document.getElementById('result').innerHTML = '0';
+    result = '';
+
 }
